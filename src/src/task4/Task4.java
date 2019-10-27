588b8a6ccaf2832704a4f10cf16591d41aef0ccb
Валентин <mila.tequilla@yandex.ru>
2019-10-27 13:17:11 +0300

    Task4 completed

--- src/src/task4/Task4.java
+++ src/src/task4/Task4.java
@@ -1,4 +1,16 @@
 package task4;
 
+import java.util.Scanner;
+/*
+Задача №4: Отработка поразрядных операций
+Задание:   Ввести с консоли число в бинарном формате.
+        Перевести его в десятичный формат, записать в переменную int и вывести на экран.
+*/
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        String b = read.nextLine();
+        int decimal = Integer.parseInt(b,2);
+        System.out.println(decimal);
+    }
 }
