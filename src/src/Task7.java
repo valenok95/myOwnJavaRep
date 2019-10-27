4dee8e2732f1d902714b098f395114a467ae914b
Валентин <mila.tequilla@yandex.ru>
2019-10-27 14:20:59 +0300

    Task5 Completed

--- src/src/Task7.java
+++ src/src/Task7.java
@@ -6,6 +6,7 @@
         и вывести на экран сообщение "Данное значение имеется в константах" если number=X или Y или Z.
         Если переменная отличается от констант, нужно вывести оповещение "Такой константы нет!"
 */
+
 import java.util.Scanner;
 
 public class Task7 {
@@ -15,10 +16,11 @@ public class Task7 {
         final int Z = 20;
         Scanner read = new Scanner(System.in);
         int number = read.nextInt();
-        if (number!=X & number!=Y & number!=Z){
-            System.out.println("Такой константы нет!");}
-            else {
-            System.out.println("Данное значение имеется в константах");}
+        if (number != X & number != Y & number != Z) {
+            System.out.println("Такой константы нет!");
+        } else {
+            System.out.println("Данное значение имеется в константах");
+        }
     }
-    }
+}
 
--- src/src/Task8.java
+++ src/src/Task8.java
@@ -5,8 +5,7 @@
 public class Task8 {
     public static void main(String[] args) {
         int i;
-        for (i=1;i<=50;i++)
-        {
+        for (i = 1; i <= 50; i++) {
             System.out.println(i);
         }
     }
