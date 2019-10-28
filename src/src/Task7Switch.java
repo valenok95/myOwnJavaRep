e7c4b0dc9e5b4f9f15030ef0cfaed9387c70158d
Валентин <mila.tequilla@yandex.ru>
2019-10-29 00:18:27 +0300

    Task7(switch) Completed!

---
+++ src/src/Task7Switch.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task7Switch {
+
+    public static void main(String[] args) {
+        final int X = 10;
+        final int Y = 15;
+        final int Z = 20;
+        Scanner read = new Scanner(System.in);
+
+        int number = read.nextInt();
+        switch (number) {
+            case X: {
+            }
+            case Y: {
+            }
+            case Z: {
+                System.out.println("Данное значение имеется в константах");
+                break;
+            }
+            default: {
+                System.out.println("Такой константы нет!");
+            }
+
+
+        }
+
+    }
+}
