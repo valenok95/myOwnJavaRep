b5ccc5212d4b2bfd1cd36dc718d5435950a92934
Валентин <mila.tequilla@yandex.ru>
2019-10-29 00:24:10 +0300

    Task6 reCompleted! поменял int на double, убрал лишний метод и лишние переменные.

--- src/src/Task6.java
+++ src/src/Task6.java
@@ -12,18 +12,12 @@ import java.util.Scanner;
 public class Task6 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int x = read.nextInt();
-        int y = read.nextInt();
-        int z = read.nextInt();
-        int sr = sredneeIs3(x, y, z);
-        int half = sr / 2;
-        System.out.println(sredneeIs3(x, y, z));
-        if (half > 3) {
+        double x = read.nextInt();
+        double y = read.nextInt();
+        double z = read.nextInt();
+        System.out.println((x + y + z) / 3);
+        if (((x + y + z) / 3) / 2 > 3) {
             System.out.println("Программа выполнена корректно");
         }
-    }
-
-    public static int sredneeIs3(int a, int b, int c) {
-        return (a + b + c) / 3;
     }
 }
