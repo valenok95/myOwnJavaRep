14dc20e3596b357df3cfb741379305af6ec6c370
Валентин <mila.tequilla@yandex.ru>
2019-10-29 16:57:31 +0300

    Task17 not completed=(

---
+++ src/src/Task17.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+Задача №17: Запись данных в файлЗадание
+        Необходимо написать программу, которая будет записывать текстовые данные, введенные с экрана, в файл .txt.
+        Запись ввести в файл до тех пор, пока не будет введена команда стоп в консоли.
+        Пользователь при тестировании программы по правилу черного ящика должен понимать,
+        какой принцип остановки записи в файл он должен подать.
+*/
+
+public class Task17 {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src\\resources\\test.txt"));
+        //PrintWriter toTxtFile = new PrintWriter("src\\resources\\test.txt");
+        writer.write(123);
+    /*    String text = null;
+        while ( text != "stop") {
+            text = scan.nextLine();
+            toTxtFile.println(text);*/
+    }
+}
+
