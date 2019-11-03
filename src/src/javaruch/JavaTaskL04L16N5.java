e12a97f207d91f675edffb6ab523532a1ef75a75
Валентин <mila.tequilla@yandex.ru>
2019-11-03 11:22:34 +0300

    javarush tasks(just in case)

---
+++ src/src/javaruch/JavaTaskL04L16N5.java
@@ -0,0 +1,14 @@
+package javaruch;
+
+import java.util.Scanner;
+
+public class JavaTaskL04L16N5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name =scan.nextLine();
+        int d = scan.nextInt();
+        int m = scan.nextInt();
+        int y = scan.nextInt();
+        System.out.println("Меня зовут "+name+"\nЯ родился "+d+"."+m+"."+y);
+    }
+}
