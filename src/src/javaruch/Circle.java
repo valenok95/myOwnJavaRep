35177a039170c8f7050d6e0007b46370b3817ee8
valenok95 <mila.tequilla@yandex.ru>
2019-11-06 23:43:23 +0400

    круглая хрень

---
+++ src/src/javaruch/Circle.java
@@ -0,0 +1,27 @@
+package src.javaruch;
+
+public class Circle {
+    public Color color;
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.color.setDescription("Red");
+        System.out.println(circle.color.getDescription());
+    }
+
+    public Circle() {
+        color = new Color();
+    }
+
+    public class Color {
+        String description;
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+}
