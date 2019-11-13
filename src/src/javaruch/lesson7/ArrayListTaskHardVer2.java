ed5f408cd37373bfb95fb653746a332624168af0
valenok95 <mila.tequilla@yandex.ru>
2019-11-14 00:56:27 +0400

    ArrayListTaskHard(Ver.2) исправлено несколько замечаний по задачке

--- src/src/javaruch/lesson7/ArrayListTaskHardVer2.java
+++ src/src/javaruch/lesson7/ArrayListTaskHardVer2.java
@@ -6,30 +6,31 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class ArrayListTaskHardVer2 {
-    static int indexMax, indexMin;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < 10; i++) {
-            list.add(read.readLine());
+            list.add(reader.readLine());
         }
-        getFirstPick(list);
+        System.out.println(getFirstPick(list));
     }
 
-    // getFirstPick;
-    public static void getFirstPick(ArrayList<String> list) {
-        int maxLength=list.get(list.size()-1).length();
-        int minLength=list.get(list.size()-1).length();
-        for (int i=list.size()-1;i>=0;i--){
-            if(maxLength<list.get(i).length()){
-                maxLength=list.get(i).length();
-                indexMax=i;
-            } else if (minLength>list.get(i).length()){
-                minLength=list.get(i).length();
-                indexMin=i;
+    // getFirstPick вернуть первое из пиковых значений.
+    public static String getFirstPick(ArrayList<String> list) {
+        int indexMax = list.size();
+        int indexMin = list.size();
+        int maxLength = list.get(list.size() - 1).length();
+        int minLength = list.get(list.size() - 1).length();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (maxLength < list.get(i).length()) {
+                maxLength = list.get(i).length();
+                indexMax = i;
+            } else if (minLength > list.get(i).length()) {
+                minLength = list.get(i).length();
+                indexMin = i;
             }
         }
-        System.out.println(indexMax>indexMin ? list.get(indexMin):list.get(indexMax));
-        }
+        return (indexMax > indexMin ? list.get(indexMin) : list.get(indexMax));
     }
+}
